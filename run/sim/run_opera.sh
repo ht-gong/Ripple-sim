922cd7999e3169e8b7d0d8f20fe15bce403272a8
Jialong Li <thu199401@gmail.com>
2024-07-01 08:50:11 +0200

    Delete run/sim directory

--- run/sim/run_opera.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#../../src/opera/datacenter/htsim_dctcp_dynexpTopology -simtime 0.1001 -cutoff 1500000000000 -cwnd 20 -q 300 -topfile ../../topologies/dynexp_N=108_k=12_1path.txt -flowfile ../traffic_gen/flows_1percLoad_1sec_648hosts_100Gbps.htsim > opera_20perc.txt
-
-../../src/opera/datacenter/htsim_ndp_dynexpTopology -simtime 0.0101 -cutoff 15000000000 -rlbflow 0 -cwnd 20 -q 80 -utiltime 5e-4 -pullrate 1 -topfile ../../topologies/dynexp_N=108_k=12_1path.txt -flowfile ../traffic_gen/websearch_20percLoad_1sec_648hosts_100Gbps.htsim > opera_20perc.txt
--- run/sim/run_rotorlb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-../../src/opera/datacenter/htsim_ndp_dynexpTopology -simtime 0.50001 -cutoff 0 -rlbflow 0 -cwnd 20 -q 65 -pullrate 1 -topfile ../../topologies/dynexp_50us_10nsrc.txt -utiltime 2e-05 -flowfile ../traffic_gen/websearch_uniform_40percLoad_1sec_648hosts.htsim > vlb_rotorlb_40perc_ws.txt &
-
