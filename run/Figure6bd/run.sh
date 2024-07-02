d673b661f707aaa168cd9f3d62c9b980e0c3f299
Haotian Gong <hgong@inet-p4lab-13.mpi-inf.mpg.de>
2024-07-02 19:59:27 +0200

    updated shell scripts

--- run/Figure6bd/run.sh
+++ run/Figure6bd/run.sh
@@ -9,3 +9,11 @@
 ../../src/optiroute/datacenter/htsim_dctcp_dynexpTopology -utiltime 5e-04 -simtime 0.801 -cutoff 150000000000 -rlbflow 0 -cwnd 20 -q 300 -routing KShortest -topfile ../../topologies/general_from_dynexp_N=108_topK=1.txt -flowfile ../../traffic/datamining_uniform_40percLoad_0.5sec_648hosts.htsim -dctcpmarking 32 -slicedur 50000000 > ksp_topK=1_datamining_dctcp_0.5sec_0.8sec_50us.txt
 
 ../../src/optiroute/datacenter/htsim_dctcp_dynexpTopology -utiltime 5e-04 -simtime 0.801 -cutoff 150000000000 -rlbflow 0 -cwnd 20 -q 300 -routing KShortest -topfile ../../topologies/general_from_dynexp_N=108_topK=5.txt -flowfile ../../traffic/datamining_uniform_40percLoad_0.5sec_648hosts.htsim -dctcpmarking 32 -slicedur 50000000 > ksp_topK=5_datamining_dctcp_0.5sec_0.8sec_50us.txt
+
+
+python3 set_config.py
+
+python3 FCT.py
+
+python3 BE_CDF.py
+python3 BE_BAR.py
